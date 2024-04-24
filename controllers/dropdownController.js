c2575201a261fef539c95cb0f5dd0019ee5e9089
Hemant Jha <156426952+DevHemantJha@users.noreply.github.com>
2024-04-24 12:47:35 +0530

    Added new test2 route

--- controllers/dropdownController.js
+++ controllers/dropdownController.js
@@ -8,7 +8,15 @@ const test = async (req, res, next) => {
     res.status(400).send(e.message);
   }
 };
+const test2 = async (req, res, next) => {
+  try {
+    // console.log("Browser/tab closed");
+    res.send('api is running for test2');
+  } catch (e) {
+    res.status(400).send(e.message);
+  }
+};
 
 module.exports = {
-  test,
+  test, test2
 };
--- routes/routes.js
+++ routes/routes.js
@@ -1,8 +1,31 @@
 const express = require('express');
 const router = express.Router();
-const { test } = require('../controllers/dropdownController');
+const { test, test2 } = require('../controllers/dropdownController');
 
 //Testing route
+/**
+ * @openapi
+ * /test:
+ *   get:
+ *     summary: Testing route
+ *     operationId: TestingRoute
+ *     responses:
+ *       "200":
+ *         description: Successful operation
+ */
 router.get('/test', test);
 
+/**
+ * @openapi
+ * /test2:
+ *   get:
+ *     summary: Testing route
+ *     operationId: TestingRoute
+ *     responses:
+ *       "200":
+ *         description: Successful operation
+ */
+router.get('/test2', test2);
+
 module.exports = router;
+
