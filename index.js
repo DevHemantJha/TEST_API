374c4c6ccad5f7d17998cfabc27f6450e4408d9c
Hemant Jha <156426952+DevHemantJha@users.noreply.github.com>
2024-04-24 10:35:10 +0530

    Fixed the validator

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
 const port = 8080;
-
+const OpenApiValidator = require('express-openapi-validator');
 const routes = require('./routes/routes');
 
 app.use(express.json());
 app.use(routes);
-
+app.use(
+  OpenApiValidator.middleware({
+    apiSpec: './openapi.yaml',
+    validateRequests: true, // (default)
+    validateResponses: true, // false by default
+  }),
+);
+app.use((err, req, res, next) => {
+  // format error
+  res.status(err.status || 500).json({
+    message: err.message,
+    errors: err.errors,
+  });
+});
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const { test } = require('../controllers/dropdownController');
 
 //Testing route
