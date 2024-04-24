5a0d7812ca56542fb2fe5171dc27071f3afba351
Hemant Jha <156426952+DevHemantJha@users.noreply.github.com>
2024-04-24 14:39:36 +0530

    Testing route removed

--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,7 @@ const router = express.Router();
 const { test, test2 } = require('../controllers/dropdownController');
 
 
-router.get('/test_Api', function(req, res) {
-    res.send("you've reached the API endpoint");
-  });
-router.get('/test_Api/test1', test);
-router.get('/test_Api/test1', test2);
+router.get('/testApi/test1', test);
+router.get('/testApi/test2', test2);
 
 module.exports = router;
