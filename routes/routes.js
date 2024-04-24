7899277bfd6dcd8b3e61ffac9e383430f574bd3f
Hemant Jha <156426952+DevHemantJha@users.noreply.github.com>
2024-04-24 14:54:39 +0530

    Fix 5

--- routes/routes.js
+++ routes/routes.js
@@ -3,10 +3,10 @@ const router = express.Router();
 const { test, test2 } = require('../controllers/dropdownController');
 
 
-router.get('/test_Api', function(req, res) {
+router.get('/test_Api', function (req, res) {
     res.send("you've reached the API endpoint");
-  });
+});
 router.get('/test_Api/test1', test);
-router.get('/test_Api/test1', test2);
+router.get('/test_Api/test2', test2);
 
 module.exports = router;
