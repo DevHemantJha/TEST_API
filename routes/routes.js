9cb889de35dac7a608011efa0bc6522ba4b9f87e
Hemant Jha <156426952+DevHemantJha@users.noreply.github.com>
2024-04-24 13:33:46 +0530

    Changes the route path

--- routes/routes.js
+++ routes/routes.js
@@ -6,31 +6,7 @@ const { test, test2 } = require('../controllers/dropdownController');
 router.get('/test_Api', function(req, res) {
     res.send("you've reached the API endpoint");
   });
-
-
-//Testing route
-/**
- * @openapi
- * /test_Api/test1:
- *   get:
- *     summary: Testing route
- *     operationId: TestingRoute
- *     responses:
- *       "200":
- *         description: Successful operation
- */
 router.get('/test_Api/test1', test);
-
-/**
- * @openapi
- * /test_Api/test2:
- *   get:
- *     summary: Testing route
- *     operationId: TestingRoute
- *     responses:
- *       "200":
- *         description: Successful operation
- */
 router.get('/test_Api/test1', test2);
 
 module.exports = router;
